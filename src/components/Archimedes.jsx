0acf5edcda871dd6a6b61ab56c66392d7a057b5d
Johnny McNeil <johnnymmcneil@gmail.com>
2023-11-02 15:12:06 -0700

    Initializing repository

---
+++ src/components/Archimedes.jsx
@@ -0,0 +1,94 @@
+'use client';
+
+import React from 'react';
+import { useGLTF, useTexture } from '@react-three/drei';
+
+export const Archimedes = React.forwardRef(function Archimedes(props, ref) {
+  const { nodes, materials } = useGLTF('/archimedes.glb');
+  return (
+    <group
+      {...props}
+      ref={ref}
+      dispose={null}
+      position={[-20, 0, 0]}
+      rotation={[0, 90, 0]}
+    >
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Back.geometry}
+        material={materials.BackBeige}
+        position={[0, -8.491, -2.673]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes['CD-ROM_Tray'].geometry}
+        material={materials.CDBeige}
+        position={[1.158, -7.05, 8.128]}
+        rotation={[0.029, 0, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Exhaust_Cover.geometry}
+        material={materials.BackBeige}
+        position={[8, -3.091, -2.271]}
+        scale={[0.063, 0.125, 3]}
+      />
+      <group position={[0, -8.491, 5.346]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Front_1.geometry}
+          material={materials.FrontBeige}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Front_2.geometry}
+          material={materials.FrontGrey}
+        />
+      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Glass.geometry}
+        material={materials.glass}
+        position={[0, -8.491, -2.805]}
+      />
+      <group
+        position={[5.452, -7.05, 8.149]}
+        rotation={[-0.01, 0, 0]}
+      >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Front002.geometry}
+          material={materials.ButtonBeige}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Front002_1.geometry}
+          material={materials.LED}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Front002_2.geometry}
+          material={materials.ClearBluePlastic}
+        />
+      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Screen.geometry}
+        material={materials.Screen}
+        position={[0, -8.491, -2.919]}
+      />
+    </group>
+  );
+});
+
+useGLTF.preload('/archimedes.glb');
---
+++ src/components/Geo.jsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Archimedes } from './Archimedes';
+
+import { createPortal } from 'react-dom';
+
+export const Geo = () => {
+  const archRef = useRef(null);
+  const mouseMove = (e) => {
+    e.stopPropagation();
+
+    const x = e.clientX;
+    const y = e.clientY;
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+
+    const xNorm = (x - w / 2) / w;
+    const yNorm = (y - h / 2) / h;
+
+    archRef.current.rotation.x = yNorm;
+    archRef.current.rotation.y = xNorm;
+  };
+
+  return (
+    <>
+      {createPortal(
+        <div className="absolute min-h-screen min-w-screen top-0 bottom-0 left-0 right-0 -z-10">
+          <Canvas camera={{ fov: 60, position: [20, 0, 20], rotatey: 45 }}>
+            <OrbitControls
+              autoRotate={true}
+              autoRotateSpeed={0.5}
+              enablePan={false}
+              enableRotate={false}
+              enableZoom={false}
+              reverseOrbit={true}
+            />
+            <ambientLight
+              color={0xffffff}
+              intensity={2}
+            />
+            <directionalLight
+              color="white"
+              position={[0, 0, 5]}
+            />
+            <Archimedes ref={archRef} />
+          </Canvas>
+        </div>,
+        document.body
+      )}
+    </>
+  );
+};
